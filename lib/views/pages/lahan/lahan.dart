fd400edca54cc4f4c90c5f86a1eb7769ea21d83c
haris ardian <harisfirman387@gmail.com>
2024-05-02 13:31:04 +0700

    feat:lahan page

---
+++ lib/views/pages/lahan/lahan.dart
@@ -0,0 +1,96 @@
+  import 'package:cloud_firestore/cloud_firestore.dart';
+  import 'package:flutter/material.dart';
+  import 'package:siop_ppl_agro/services/lahan_services.dart';
+  import 'package:siop_ppl_agro/views/pages/lahan/addLahan.dart';
+  import 'package:siop_ppl_agro/views/pages/sensor/sensor.dart';
+
+  class HomeLahan extends StatefulWidget {
+    const HomeLahan({super.key});
+
+    @override
+    State<HomeLahan> createState() => _HomeLahanState();
+  }
+
+  class _HomeLahanState extends State<HomeLahan> {
+    Stream? _LahanSteam;
+
+    getontheload() async {
+      _LahanSteam = LahanServices().getLahan();
+      setState(() {});
+    }
+
+    @override
+    void initState() {
+      getontheload();
+      super.initState();
+    }
+
+    Widget allLahan() {
+      return StreamBuilder(
+          stream: _LahanSteam,
+          builder: (BuildContext context, AsyncSnapshot snapshot) {
+            return snapshot.hasData
+                ? ListView.builder(
+                    itemCount: snapshot.data.docs.length,
+                    itemBuilder: (context, index) {
+                      DocumentSnapshot ds = snapshot.data.docs[index];
+                      return GestureDetector(
+                        onTap: () {
+                          Navigator.push(
+                            context,
+                            MaterialPageRoute(
+                                builder: (context) => SensorPage(
+                                      lahanId: ds["Id"],
+                                      lahanName: ds["Lahan"],
+                                    )),
+                          );
+                        },
+                        child: Container(
+                          margin: const EdgeInsets.only(top: 10, bottom: 10),
+                          width: MediaQuery.of(context).size.width,
+                          decoration: BoxDecoration(
+                              border: Border.all(width: 1),
+                              borderRadius: BorderRadius.circular(5)),
+                          child: Column(
+                            children: [
+                              Text(ds["Id"].toString()),
+                              Text(ds["Lahan"].toString()),
+                              Text("Umur tanaman :${ds["Umur"]}"),
+                            ],
+                          ),
+                        ),
+                      );
+                    })
+                : Container();
+          });
+    }
+
+    @override
+    Widget build(BuildContext context) {
+      return Scaffold(
+        appBar: AppBar(
+          title: const Text("Lahanku"),
+        ),
+        body: Container(
+          padding: const EdgeInsets.all(15),
+          child: Column(
+            children: [
+              Expanded(
+                child: allLahan(),
+              ),
+              ElevatedButton(
+                  onPressed: () {
+                    Navigator.push(
+                      context,
+                      MaterialPageRoute(
+                        builder: (context) => AddLahan(),
+                      ),
+                    );
+                  },
+                  child: Text("Tambah lahan"))
+            ],
+          ),
+        ),
+      );
+    }
+  }

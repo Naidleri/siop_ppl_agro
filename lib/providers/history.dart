8d865be5aa8d1672c3d0dbbb199fb8c01f8d242d
haris ardian <harisfirman387@gmail.com>
2024-05-11 22:22:19 +0700

    feat:add history feat

--- lib/providers/history.dart
+++ lib/providers/history.dart
@@ -1,3 +1,4 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
 import 'package:siop_ppl_agro/models/historyModel.dart';
 import 'package:siop_ppl_agro/services/history_services.dart';
@@ -15,9 +16,51 @@ class SensorHistoryProvider extends ChangeNotifier {
       final key = event.snapshot.key!;
       final data = event.snapshot.value as Map<dynamic, dynamic>;
       final servo = data['servo'] as int;
+
+      if (servo == 90) {
+        sendDataToFirestore(key, servo, data);
+      }
+
       _sensorDataList.add(SensorHistoryData(key, servo, data));
       _sensorCount++;
       notifyListeners();
     });
+  }
+  Future<void> sendDataToFirestore(
+      String key, int servo, Map<dynamic, dynamic> data) async {
+    try {
+      DateTime now = DateTime.now();
+      String formattedDate =
+          '${now.day}-${now.month}-${now.year} ${now.hour}:${now.minute}:${now.second}';
+      await FirebaseFirestore.instance.collection("history").doc(key).set({
+        'date': formattedDate,
+        'servo': servo,
+      });
+    } catch (error) {
+      print("$error");
+    }
+  }
+
+  void monitorServoValue(String key, int servo) {
+    if (servo == 90) {
+      updateHistory(key, servo);
+    }
+  }
+
+  Future<void> updateHistory(
+    String key,
+    int servo,
+  ) async {
+    try {
+      DateTime now = DateTime.now();
+      String formattedDate =
+          '${now.day}-${now.month}-${now.year} ${now.hour}:${now.minute}:${now.second}';
+      await FirebaseFirestore.instance.collection("history").doc(key).update({
+        'date': formattedDate,
+        'servo': servo,
+      });
+    } catch (error) {
+      print("$error");
+    }
   }
 }

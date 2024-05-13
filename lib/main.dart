846173f906c96e7ae4bd368c547a3fe74da3f9a1
haris ardian <harisfirman387@gmail.com>
2024-05-13 14:10:02 +0700

    feat:delete unused line

--- lib/main.dart
+++ lib/main.dart
@@ -1,12 +1,7 @@
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
-import 'package:provider/provider.dart';
 import 'package:siop_ppl_agro/firebase_options.dart';
-import 'package:siop_ppl_agro/models/historyModel.dart';
-import 'package:siop_ppl_agro/providers/history.dart';
-import 'package:siop_ppl_agro/providers/lahan.dart';
-import 'package:siop_ppl_agro/providers/sensor.dart';
 import 'package:siop_ppl_agro/views/pages/lahan/lahan.dart';
 import 'package:siop_ppl_agro/views/pages/sensor/history.dart';
 import 'package:siop_ppl_agro/views/pages/sensor/sensor.dart';
@@ -53,7 +48,7 @@ class MyApp extends StatelessWidget {
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
         useMaterial3: true,
       ),
-      home: HistoryPage(),
+      home: HomeLahan(),
     );
   }
 }

8d2adaef95e946ac2a13a0bd9ac3b5a53afe7703
haris ardian <harisfirman387@gmail.com>
2024-05-14 16:23:47 +0700

    feat:delete bloc init in main

--- lib/main.dart
+++ lib/main.dart
@@ -1,34 +1,16 @@
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:provider/provider.dart';
 import 'package:siop_ppl_agro/firebase_options.dart';
+import 'package:siop_ppl_agro/providers/lahan.dart';
+import 'package:siop_ppl_agro/providers/sensor.dart';
 import 'package:siop_ppl_agro/views/pages/lahan/lahan.dart';
 import 'package:siop_ppl_agro/views/pages/sensor/history.dart';
 import 'package:siop_ppl_agro/views/pages/sensor/sensor.dart';
 
-class SimpleBlocObserver extends BlocObserver {
-  @override
-  void onEvent(Bloc bloc, Object? event) {
-    super.onEvent(bloc, event);
-    print('Event: $event');
-  }
-
-  @override
-  void onTransition(Bloc bloc, Transition transition) {
-    super.onTransition(bloc, transition);
-    print('Transition: $transition');
-  }
-
-  @override
-  void onError(BlocBase bloc, Object error, StackTrace stackTrace) {
-    super.onError(bloc, error, stackTrace);
-    print('Error: $error');
-  }
-}
-
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
-  Bloc.observer = SimpleBlocObserver();
   await Firebase.initializeApp(
     options: DefaultFirebaseOptions.currentPlatform,
   );
@@ -38,17 +20,22 @@ void main() async {
 }
 
 class MyApp extends StatelessWidget {
-  const MyApp({super.key});
+  const MyApp({Key? key});
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'PPl-Agro SIOP',
-      theme: ThemeData(
-        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
-        useMaterial3: true,
+    return MultiProvider(
+      providers: [
+        ChangeNotifierProvider(create: (context) => LahanProvider()),
+      ],
+      child: MaterialApp(
+        title: 'PPl-Agro SIOP',
+        theme: ThemeData(
+          colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+          useMaterial3: true,
+        ),
+        home: HomeLahan(),
       ),
-      home: HomeLahan(),
     );
   }
 }

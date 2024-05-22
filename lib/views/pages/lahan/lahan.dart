56f2ed7f6715439b9f72d5d66727d9904cd23838
haris ardian <harisfirman387@gmail.com>
2024-05-22 22:19:01 +0700

    feat:delete import unused

--- lib/views/pages/lahan/lahan.dart
+++ lib/views/pages/lahan/lahan.dart
@@ -1,5 +1,4 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_local_notifications/flutter_local_notifications.dart';
 import 'package:provider/provider.dart';
 import 'package:siop_ppl_agro/providers/lahan.dart';
 import 'package:siop_ppl_agro/views/pages/lahan/addLahan.dart';
